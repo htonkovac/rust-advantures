fbf38b5a6dbedcd1d34a3499a14e2e591ea8fd74
Hrvoje Tonkovac <hrvoje.tonkovac@toptal.com>
2022-10-30 11:36:04 +0100

    hw

---
+++ hello-world/target/debug/deps/rust_advantures-8695609e911cfe65.d
@@ -0,0 +1,5 @@
+/Users/hrvojetonkovac/H17Consulting/rust-advantures/hello-world/target/debug/deps/rust_advantures-8695609e911cfe65: main.rs
+
+/Users/hrvojetonkovac/H17Consulting/rust-advantures/hello-world/target/debug/deps/rust_advantures-8695609e911cfe65.d: main.rs
+
+main.rs:
---
+++ hello-world/target/debug/rust-advantures.d
@@ -0,0 +1,1 @@
+/Users/hrvojetonkovac/H17Consulting/rust-advantures/hello-world/target/debug/rust-advantures: /Users/hrvojetonkovac/H17Consulting/rust-advantures/hello-world/main.rs
